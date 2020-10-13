7f750da20ad35fd2ea61aa4088c3ed7ff8634cd5
Kimberly Horany <kim_horany@Kimberlys-MBP.attlocal.net>
2020-10-13 17:03:04 -0500

    adding election analysis

--- PyPoll.py
+++ PyPoll.py
@@ -13,7 +13,7 @@ import csv
 file_to_load = os.path.join("Resources","election_results.csv")
 
 #assign variable to save file to path
-file_to_save = os.path.join("analsis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 #initialize total vote counter
 total_votes = 0
@@ -55,24 +55,41 @@ with open(file_to_load) as election_data:
         #add vote to candidate count
         candidate_votes[candidate_name] += 1
 
-for candidate_name in candidate_votes:
-    votes = candidate_votes[candidate_name]
-    vote_percentage = (float(votes) / float(total_votes)) * 100
-    #print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
+# Save the results to our text file.
+with open(file_to_save, "w") as txt_file:        
 
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate_name
+    # Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = (float(votes) / float(total_votes)) * 100
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        #write results to text file
+        txt_file.write(candidate_results)
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+            
         
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    
-winning_candidate_summary = (
-f"-------------------------\n"
-f"Winner: {winning_candidate}\n"
-f"Winning Vote Count: {winning_count:,}\n"
-f"Winning Percentage: {winning_percentage:.1f}%\n"
-f"-------------------------\n")
-print(winning_candidate_summary)
+        
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+
+    # Save the winning candidate's name to the text file.
+    txt_file.write(winning_candidate_summary)
 
